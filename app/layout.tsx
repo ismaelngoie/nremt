a9d5544fcec62b8ac7faf77d95f00b5468f888a7
Ismael Ngoie <ismael@ngoie.com>
2026-01-17 11:31:51 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,17 @@ export default function RootLayout({
             gtag('config', 'AW-17883612588');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "v2xt1zqmjt");
+          `}
+        </Script>
       </head>
 
       <body className={inter.className}>{children}</body>
